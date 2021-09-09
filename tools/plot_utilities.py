45e01cc46f3764666a1b57f8897df97ac297532b
gaffarelj <j.y.p.gaffarel@student.tudelft.nl>
2021-09-09 15:13:20 +0200

    Added more plotting options

--- tools/plot_utilities.py
+++ tools/plot_utilities.py
@@ -5,6 +5,60 @@ matplotlib.use("pdf")
 import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 13, 'figure.figsize': (10.5, 7), 'savefig.format': 'pdf'})
 
+def plot_single(x_data, y_data, x_label, y_label, fname, xlog=False, ylog=False):
+    """
+    Simple plot
+    """
+    fig, ax = plt.subplots()
+    # Plot
+    ax.plot(x_data, y_data)
+    # Set labels
+    ax.set_xlabel(x_label), ax.set_ylabel(y_label)
+    # Save space
+    fig.tight_layout()
+    ax.grid()
+    # Set log axis if requested
+    if xlog:
+        plt.xscale("log")
+    if ylog:
+        plt.yscale("log")
+    # Save the plot in the figure folder, as a pdf
+    plt.savefig("figures/%s.pdf" % fname)
+
+def plot_multiple(x_datas, y_datas, x_label, y_label, fname, legends=None, colors=None, xlog=False, ylog=False, ylim=None, xlim=None):
+    """
+    Plot multiple lines
+    """
+    fig, ax = plt.subplots()
+    # Plot
+    for i in range(len(x_datas)):
+        # If specified, use the appropriate color
+        if colors is not None and len(colors) > i:
+            ax.plot(x_datas[i], y_datas[i], color=colors[i])
+        else:
+            ax.plot(x_datas[i], y_datas[i])
+    # Set labels
+    ax.set_xlabel(x_label), ax.set_ylabel(y_label)
+    # Set legend
+    if legends is not None:
+        ax.legend(legends)
+    # Save space
+    fig.tight_layout()
+    ax.grid()
+    # Set log axis if requested
+    if xlog:
+        plt.xscale("log")
+    if ylog:
+        plt.yscale("log")
+    # Set the axis limits if requested
+    if ylim is not None:
+        ax.set_ylim(ylim)
+    if xlim is not None:
+        ax.set_xlim(xlim)
+    # Save the plot in the figure folder, as a pdf
+    plt.savefig("figures/%s.pdf" % fname)
+
+
 def plot_dual(x_data, y_data_1, y_data_2, x_label, y_label_1, y_label_2, fname):
     """
     Plot with two y axis. Inputs should be self-explanatory.
@@ -31,3 +85,4 @@ def plot_dual(x_data, y_data_1, y_data_2, x_label, y_label_1, y_label_2, fname):
     fig.tight_layout()
     # Save the plot in the figure folder, as a pdf
     plt.savefig("figures/%s.pdf" % fname)
+
