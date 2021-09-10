ef2a22dbf28469c281f8ad8e41020609a875be66
gaffarelj <j.y.p.gaffarel@student.tudelft.nl>
2021-09-10 11:02:10 +0200

    Tested that the MCD file are kept loaded between distinct propagations

--- setup_selection/environments/test_MCD_atmosphere.py
+++ setup_selection/environments/test_MCD_atmosphere.py
@@ -7,7 +7,7 @@ import tools.plot_utilities as PU
 import tools.time_conversions as TC
 import numpy as np
 
-simulation_days = 20
+simulation_days = 2
 simulation_start_epoch = TC.MCD_to_Tudat(2459942)
 simulation_end_epoch = simulation_start_epoch + simulation_days*constants.JULIAN_DAY
 
@@ -15,31 +15,34 @@ simulation_end_epoch = simulation_start_epoch + simulation_days*constants.JULIAN
 bodies, bodies_to_propagate, central_bodies = SU.create_bodies(use_MCD_atmo=True)
 # Define the accelerations to be included
 acceleration_models = SU.setup_environment(bodies, bodies_to_propagate, central_bodies)
-# Define the initial state of the satellite
-initial_state = SU.get_initial_state(bodies, altitude=200e3)
-# Define the termination settings
-termination_settings = SU.simulation_settings(simulation_end_epoch)
-# Define the dependent variables to save
-dependent_variables_to_save = [
-    propagation_setup.dependent_variable.altitude("Satellite", "Mars"),
-    propagation_setup.dependent_variable.density("Satellite", "Mars")
-]
 
-# Define the integrator settings
-integrator_settings = SU.get_best_integrator(simulation_start_epoch)
-# Propagator settings
-propagator_settings = propagation_setup.propagator.translational(
-    central_bodies,
-    acceleration_models,
-    bodies_to_propagate,
-    initial_state,
-    termination_settings,
-    propagation_setup.propagator.unified_state_model_quaternions,
-    dependent_variables_to_save
-)
+for h in [200e3, 250e3, 300e3]:
+    print("Simulate for h = %i m" % (h/1e3))
+    # Define the initial state of the satellite
+    initial_state = SU.get_initial_state(bodies, altitude=h)
+    # Define the termination settings
+    termination_settings = SU.simulation_settings(simulation_end_epoch)
+    # Define the dependent variables to save
+    dependent_variables_to_save = [
+        propagation_setup.dependent_variable.altitude("Satellite", "Mars"),
+        propagation_setup.dependent_variable.density("Satellite", "Mars")
+    ]
 
-# Run the simulation
-time, altitudes, densities, cpu_time = SU.run_simulation(bodies, integrator_settings, propagator_settings)
+    # Define the integrator settings
+    integrator_settings = SU.get_best_integrator(simulation_start_epoch)
+    # Propagator settings
+    propagator_settings = propagation_setup.propagator.translational(
+        central_bodies,
+        acceleration_models,
+        bodies_to_propagate,
+        initial_state,
+        termination_settings,
+        propagation_setup.propagator.unified_state_model_quaternions,
+        dependent_variables_to_save
+    )
 
-# Make plot
-PU.plot_dual(np.array(time)/3600, altitudes/1e3, densities, "Time [hr]", "Altitude [km]", "Density [kg/m$^3$]", "MCD/test_MCD_atmo")
+    # Run the simulation
+    time, altitudes, densities, cpu_time = SU.run_simulation(bodies, integrator_settings, propagator_settings)
+
+## Make plot
+#PU.plot_dual(np.array(time)/3600, altitudes/1e3, densities, "Time [hr]", "Altitude [km]", "Density [kg/m$^3$]", "MCD/test_MCD_atmo")
