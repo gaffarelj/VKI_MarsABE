e60b0b76c79b6749a190e142b0d7517e7558bc98
gaffarelj <j.y.p.gaffarel@student.tudelft.nl>
2021-09-14 10:22:56 +0200

    Added investigation of the shadowing of the Sun caused by Mars

--- setup_selection/environments/test_req_envs.py
+++ setup_selection/environments/test_req_envs.py
@@ -2,9 +2,11 @@ import sys
 sys.path.insert(0,"\\".join(sys.path[0].split("\\")[:-2]))
 from tudatpy.kernel import constants
 from tudatpy.kernel.simulation import propagation_setup
+from tudatpy.kernel.interface import spice_interface
 from setup_selection import setup_utils as SU
 from tools import plot_utilities as PU
 from tools import time_conversions as TC
+from tools import mission_geometry as MG
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -15,13 +17,13 @@ simulation_end_epoch = simulation_start_epoch + simulation_days*constants.JULIAN
 # Define the environment and bodies
 bodies, bodies_to_propagate, central_bodies = SU.create_bodies(use_MCD_atmo=False, use_MCD_winds=True)
 # Define the initial state of the satellite
-initial_state = SU.get_initial_state(bodies, altitude=450e3, inclination=np.deg2rad(90))
+initial_state = SU.get_initial_state(bodies)
 # Define the termination settings
 termination_settings = SU.simulation_settings(simulation_end_epoch)
 # Define the dependent variables to save
 dependent_variables_to_save = [
-    propagation_setup.dependent_variable.altitude("Satellite", "Mars"),
-    propagation_setup.dependent_variable.density("Satellite", "Mars")
+    propagation_setup.dependent_variable.relative_position("Satellite", "Mars"),
+    propagation_setup.dependent_variable.relative_position("Sun", "Mars")
 ]
 integrator_settings = SU.get_best_integrator(simulation_start_epoch, extra_accurate=True)
 
@@ -153,9 +155,24 @@ for i, accelerations in enumerate(accelerations_list):
     positions = np.linalg.norm(states[:,:3], axis=1)
     velocities = np.linalg.norm(states[:,3:], axis=1)
 
-    times.append(time)
+    times.append(time/3600)
     pos_t.append(np.fabs(positions-positions[0])/1e3)
     vel_t.append(np.fabs(velocities-velocities[0]))
+
+    # Check radiation pressure over time
+    if check_rad:
+        sat_pos = dep_vars[:,:3]
+        sun_pos = dep_vars[:,3:]
+        r_mars = [0, 0, 0]
+        R_mars = spice_interface.get_average_radius("Mars")
+        R_sun = spice_interface.get_average_radius("Sun")
+        shadow_vals = []
+        for i in range(len(sat_pos)):
+            r_sat, r_sun = sat_pos[i], sun_pos[i]
+            shadow = MG.shadow_function(r_sun, R_sun, r_mars, R_mars, r_sat)
+            shadow_vals.append(shadow)
+        # Make plot
+        PU.plot_single(time/3600, shadow_vals, "Time [hr]", "Shadow value [-]", "envs/radiation_shadow")
 
 # Make plot
 PU.plot_multiple(times, pos_t, "Time [hr]", "$|r(t) - r_0|$ [km]", "envs/%s_pos" % fname, legends=legends, legend_loc=4, lstyle="--")
