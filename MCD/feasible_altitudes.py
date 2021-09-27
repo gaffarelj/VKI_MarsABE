a3e7d9ccae119c58ddd5670d0946408a5b088c4c
gaffarelj <j.y.p.gaffarel@student.tudelft.nl>
2021-09-27 10:36:09 +0200

    Re-ran the feasible altitudes with the updated Cd

--- MCD/feasible_altitudes.py
+++ MCD/feasible_altitudes.py
@@ -10,7 +10,8 @@ import thrust.simple_thrust as T
 import matplotlib.pyplot as plt
 import numpy as np
 
-altitudes = np.arange(80, 220.1, 2.5)
+#altitudes = np.arange(80, 220.1, 2.5)
+altitudes = np.arange(50, 160.1, 2.5)
 #altitudes = [85, 115, 150]
 orbit_time = []
 
@@ -25,7 +26,7 @@ for h in altitudes:
     # Define the initial state of the satellite
     initial_state = SU.get_initial_state(bodies, altitude=h*1e3, inclination=np.deg2rad(0.01), eccentricity=0)
     # Define the termination settings
-    termination_settings = SU.simulation_settings(simulation_end_epoch)
+    termination_settings = SU.simulation_settings(simulation_end_epoch, end_altitude=25e3)
     # Define the dependent variables to save
     dependent_variables_to_save = [
         propagation_setup.dependent_variable.relative_speed("Satellite", "Mars"),
@@ -49,7 +50,7 @@ for h in altitudes:
     # Run the simulation
     time, states, dep_vars = SU.run_simulation(bodies, integrator_settings, propagator_settings, return_raw=True)
     orbit_time.append(time[-1]/constants.JULIAN_DAY)
-    print("Starting from altitude of %i km, stay in orbit %.1e days" % (h, time[-1]/constants.JULIAN_DAY))
+    print("Starting from altitude of %.1f km, stay in orbit %.1e days" % (h, time[-1]/constants.JULIAN_DAY))
     print("Orbital velocity of %.3f m/s, air density of %.5e kg/m3" % (np.mean(dep_vars[:10,0]), np.mean(dep_vars[:10,1])))
 
 PU.plot_single(orbit_time, altitudes, "Time in orbit [days]", "Starting altitude [km]", "MCD/feasible_altitudes", xlog=True)
