b90356e9f479488afb92a0304233411cb2665af8
gaffarelj <j.y.p.gaffarel@student.tudelft.nl>
2021-10-04 11:28:51 +0200

    Added drag print during SPARTA simulation

--- SPARTA/setup/comp_inputs.py
+++ SPARTA/setup/comp_inputs.py
@@ -149,13 +149,17 @@ for j, s_name in enumerate(sat_names):
             input_s += "compute             2 surf all all fx fy fz\n"
             input_s += "fix                 avg ave/surf all 1 5 5 c_2[*] ave running\n"
             input_s += "dump                1 surf all 5 ../results_sparta/%s/force_%skm.*.gz f_avg[*]\n" % (s_name, h)
+            input_s += "compute             sum reduce sum f_avg[*]\n"
             input_s += "\n"
             if check_part_cells:
                 input_s += "compute             npart grid all all n\n"
                 input_s += "dump                2 grid all 5 ../results_sparta/%s/npart_%skm.*.gz c_npart[*]\n" % (s_name, h)
+                #input_s += "\n"
+                #input_s += "dump                img image all 50 res.*.ppm type vx\n"
+                # change color to be compute or fix equal to sum of particules in cell (c_npart[*])
                 input_s += "\n"
             input_s += "stats               25\n"
-            input_s += "stats_style         step cpu np nscoll nscheck nexit\n"
+            input_s += "stats_style         step cpu np nscoll nexit c_sum[1]\n"
             input_s += "run                 %i\n" % (500*(3-i))
             
             run_all_cmd += "mpirun -np 16 spa_ < in.%s_%skm \n" % (s_name, h)
