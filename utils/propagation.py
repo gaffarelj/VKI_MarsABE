2f9468f0facc748169937f8d41f32d4441703d13
gaffarelj <j.y.p.gaffarel@student.tudelft.nl>
2021-10-06 11:24:17 +0200

    + integrator, propagator, termination, dep vars

--- utils/propagation.py
+++ utils/propagation.py
@@ -1,3 +1,4 @@
+import numpy as np
 import sys
 sys.path.insert(0,"/".join(sys.path[0].split("/")[:-1]))
 from tools import time_conversions as TC
@@ -42,8 +43,15 @@ class env_acceleration:
             self.a.append(propagation_setup.acceleration.cannonball_radiation_pressure())
 
 class orbit_simulation:
-    # Orbital simulation class, containing all the code required for setup, and simulation run
+    
     def __init__(self, sat, central_body, sim_time, init_time=TC.MCD_to_Tudat(2459942)):
+        """
+        Orbital simulation class, containing all the code required for setup, and simulation run.
+        Inputs:
+         * sat (utils.sat_models.satellite): satellite for which the orbits is simulation
+         * central_body (str): body around which the satellite orbits
+         * init_time (float): initial simulation time in seconds since J2000
+        """
         self.sat = sat
         self.central_body = central_body
         self.init_time = init_time
@@ -163,25 +171,195 @@ class orbit_simulation:
                     env_acceleration("Jupiter", PM=True),
                 ]
         # Convert the provided accelerations to a dict (format for TUDAT)
-        accelerations = dict()
+        self.accelerations = dict()
         for env_a in env_accelerations:
-            accelerations[env_a.body_name] = env_a.a
+            self.accelerations[env_a.body_name] = env_a.a
         # Add the thrust as an acceleration
         if thrust is not None:
-            accelerations[self.sat.name] =  [T.thrust_settings(self, thrust)]
+            self.accelerations[self.sat.name] =  [T.thrust_settings(self, thrust)]
         # Create the acceleration models
         self.acceleration_models = propagation_setup.create_acceleration_models(
             self.bodies,
-            {self.sat.name: accelerations},
+            {self.sat.name: self.accelerations},
             [self.sat.name],
             [self.central_body]
         )
 
+    def create_integrator(self, tolerance=1e-7, dt=[1e-5, 150, 600], sf=[0.85, 0.25, 3.0]):
+        """
+        Create the RK-DP 87 variable step integrator for the orbital simulation.
+        Inputs:
+         * tolerance (float): integrator tolerance, this implicitely tunes the integration steps
+         * dt ([float]*3): time step settings: 0=minimum, 1=initial, 2=maximum
+         * sf ([float]*3): safety factor settings: 0=initial, 1=minimum increase factor, 2=maximum increase factor
+        """
+        # Setup the optimal integrator settings
+        initial_time = self.init_time           # seconds since J2000
+        initial_time_step = dt[1]                 # seconds
+        coefficient_set = propagation_setup.integrator.RKCoefficientSets.rkdp_87 
+        minimum_step_size = dt[0]                # seconds
+        maximum_step_size = dt[2]                 # seconds
+        relative_error_tolerance = tolerance    # -
+        absolute_error_tolerance = tolerance    # -
+        self.integrator_settings = propagation_setup.integrator.runge_kutta_variable_step_size(
+            initial_time,
+            initial_time_step,
+            coefficient_set,
+            minimum_step_size,
+            maximum_step_size,
+            relative_error_tolerance,
+            absolute_error_tolerance,
+            save_frequency= 1,
+            assess_termination_on_minor_steps = False,
+            safety_factor = sf[0],
+            maximum_factor_increase = sf[1],
+            minimum_factor_increase = sf[2] )
 
+    def create_termination_settings(self, min_altitude=50e3, max_altitude=500e3, cpu_time=60*60):
+        """
+        Create the termination settings; conditions for when the simulation must stop.
+        Inputs:
+         * min_altitude (float): altitude below which propagation stops, in meters (set to 0 to remove)
+         * max_altitude (float): altitude above which propagation stops, in meters (set to Inf to remove)
+         * cpu_time (float): CPU time after which the propagation is stopped, in seconds (set to Inf to remove)
+        """
+        # Create a lower altitude termination setting (when sat is too deep in the atmosphere or crashed)
+        termination_altitude = propagation_setup.dependent_variable.altitude(self.sat.name, self.central_body)
+        termination_setting_min_altitude = propagation_setup.propagator.dependent_variable_termination(
+                dependent_variable_settings = termination_altitude, limit_value = min_altitude, use_as_lower_limit = True)
+        # Create an upper altitude termination setting (when sat is too high above the planet)
+        termination_setting_max_altitude = propagation_setup.propagator.dependent_variable_termination(
+                dependent_variable_settings = termination_altitude, limit_value = max_altitude, use_as_lower_limit = False)
+        # Create a time termination setting (stop after x days in orbit)
+        termination_setting_time = propagation_setup.propagator.time_termination(self.end_time)
+        # Create a CPU time termination setting (stop after x minutes of CPU time)
+        termination_setting_cpu_time = propagation_setup.propagator.cpu_time_termination(cpu_time)
+        # Assemble the termination settings together (stop after one of the termination setting is reached)
+        termination_settings_list = [termination_setting_min_altitude, termination_setting_max_altitude, termination_setting_time, termination_setting_cpu_time]
+        self.termination_settings = propagation_setup.propagator.hybrid_termination( 
+            termination_settings_list, fulfill_single_condition = True )
 
-from utils import sat_models as SM
+    def create_dependent_variables(self, to_save=[]):
+        """
+        Dependent variables to save during the propagation.
+        Inputs:
+         * to_save ([string]*N): string representing which dependent variable to save:
+           * h:     altitude of the satellite
+           * rho:   atmospheric density at the position of the satellite
+           * V:     airspeed of the Satellite
+           * m:     mass of the satellite
+           * F_T:   acceleration due to the Thrust
+           * D:     acceleration due to the atmosphere
+           * C_D:   aerodynamic coefficients
+           * r_cb:  relative position of the central body w.r.t. the sun
+           * Kep:   Keplerian state of the satellite
+        """
+        # Start with no dependent variables to save
+        self.dependent_variables_to_save = []
+        # Dictionnary to save which dependent variables can be accessed at what index
+        self.dep_var_loc = dict()
+        idx = 0
+        for dep_key in to_save:
+            if dep_key == "h":
+                # Altitude of the satellite
+                d_v = propagation_setup.dependent_variable.altitude(self.sat.name, self.central_body)
+                size = 1
+            elif dep_key == "rho":
+                # Atmospheric density at the position of the satellite
+                d_v = propagation_setup.dependent_variable.density(self.sat.name, self.central_body)
+                size = 1
+            elif dep_key == "V":
+                # Airspeed of the Satellite
+                propagation_setup.dependent_variable.body_fixed_airspeed_velocity(self.sat.name, self.central_body)
+                size = 3
+            elif dep_key == "m":
+                # Mass of the satellite
+                d_v = propagation_setup.dependent_variable.body_mass(self.sat.name)
+                size = 1
+            elif dep_key == "F_T":
+                # Acceleration due to the Thrust
+                d_v = propagation_setup.dependent_variable.single_acceleration_norm(
+                    propagation_setup.acceleration.thrust_acceleration_type, self.sat.name, self.sat.name)
+                size = 3
+            elif dep_key == "D":
+                # Acceleration due to the atmosphere
+                d_v = propagation_setup.dependent_variable.single_acceleration_norm(
+                    propagation_setup.acceleration.aerodynamic_type, self.sat.name, self.central_body)
+                size = 3
+            elif dep_key == "C_D":
+                # Aerodynamic coefficients
+                propagation_setup.dependent_variable.aerodynamic_force_coefficients(self.sat.name)
+                size = 3
+            elif dep_key == "r_cb":
+                # Relative position of the central body w.r.t. the sun
+                propagation_setup.dependent_variable.relative_position(self.sat.name, self.central_body)
+                size = 3
+            elif dep_key == "Kep":
+                # Keplerian state of the satellite
+                propagation_setup.dependent_variable.keplerian_state(self.sat.name, self.central_body)
+                size = 6
+            else:
+                # Show a warning message if the dependent variable is not known
+                list_d_v = ["h", "rho", "V", "m", "F_T", "D", "C_D", "r_cb", "Kep"]
+                print("Warning: The dependent variable '%s' is not in known list:" % dep_key, list_d_v)
+                size = 0
+            if size != 0:
+                # Add the dependent variable to the list
+                self.dependent_variables_to_save.append(d_v)
+                # Save the index at which this dependent variable can be save
+                self.dep_var_loc[dep_key] = [idx, size]
+                # Increment the index by the size of the dependent variable
+                idx += size
 
-OS = orbit_simulation(SM.satellites["CS_1021"], "Mars", 2*constants.JULIAN_DAY)
-OS.create_bodies()
-OS.create_initial_state()
-OS.create_accelerations(default_config=1, thrust=1)
+    def create_propagator(self, propagator=propagation_setup.propagator.encke, prop_mass=False):
+        """
+        Create the simulation propagator.
+        Inputs:
+         * propagator: type of propagator to use (Encke is advised; it propagates the difference with a Keplerian orbit)
+         * prop_mass (bool): whether or not to propagate the satellite mass based on thrust or not
+        """
+        # Define the translational propagator settings
+        translation_propagator = propagation_setup.propagator.translational(
+            [self.central_body],
+            self.acceleration_models,
+            [self.sat.name],
+            self.initial_state,
+            self.termination_settings,
+            propagator,
+            self.dependent_variables_to_save
+        )
+        # Also propagate the mass based on the acceleration from the thrust
+        if prop_mass:
+            # Define the mass propagator settings
+            mass_rate_model = {self.sat.name: [propagation_setup.mass.from_thrust()]}
+            mass_propagator = propagation_setup.propagator.mass(
+                [self.sat.name],
+                mass_rate_model,
+                np.array([self.sat.wet_mass]),
+                self.termination_settings)
+            # Define the full propagator settings
+            self.propagator_settings = propagation_setup.propagator.multitype(
+                [translation_propagator, mass_propagator], self.termination_settings, self.dependent_variables_to_save)
+            # The following 3 lines are needed to properly propagate the mass
+            translation_propagator.recreate_state_derivative_models(self.bodies)
+            translation_propagator.reset_and_recreate_acceleration_models({self.sat.name: self.accelerations}, self.bodies)
+            self.propagator_settings.recreate_state_derivative_models(self.bodies)
+        # Only propagate the vehicle translationally
+        else:
+            self.propagator_settings = translation_propagator
+
+    def simulate(self):
+        pass
+
+test = True
+if test:
+    from utils import sat_models as SM
+    OS = orbit_simulation(SM.satellites["CS_1021"], "Mars", 2*constants.JULIAN_DAY)
+    OS.create_bodies()
+    OS.create_initial_state()
+    OS.create_accelerations(default_config=1, thrust=1)
+    OS.create_integrator()
+    OS.create_termination_settings()
+    OS.create_dependent_variables(to_save=["m", "V", "h", "rho", "Kep", "F_T"])
+    OS.create_propagator(prop_mass=True)
+
