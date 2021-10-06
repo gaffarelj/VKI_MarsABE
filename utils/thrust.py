42e39d24f04cd0872a504b61113b8afaf183c833
gaffarelj <j.y.p.gaffarel@student.tudelft.nl>
2021-10-06 10:17:54 +0200

    Started new cleaner thrust module

---
+++ utils/thrust.py
@@ -0,0 +1,107 @@
+import numpy as np
+from tudatpy.kernel.simulation import propagation_setup
+from tudatpy.kernel.interface import spice_interface
+from tools import mission_geometry as MG
+from thrust_models import BHT_100
+
+# Astronomical Unit distance in [m]
+AU = 149597890e3
+# Solar irradiance dict
+solar_irradiances = dict()
+# Power dict
+power_dict = dict()
+
+class thrust_model:
+
+    def __init__(self, propagation, thrust_mod=0, save_power=False, solar_constant=1366):
+        self.prop = propagation
+        self.vehicle = self.prop.bodies.get_body(self.prop.sat.name)
+        self.init_time = self.prop.init_time
+        self.sat = self.prop.sat
+        self.central_body = self.prop.bodies.get_body(self.prop.central_body)
+        self.sun = self.prop.bodies.get_body("Sun")
+        self.save_power = save_power
+        self.solar_constant = solar_constant
+        self.thrust_mod = thrust_mod
+        if thrust_mod == 0:
+            self.power_treshold = [10, 100]
+            self.dens_treshold = [0, np.inf]
+        elif thrust_mod == 1:
+            self.power_treshold = [107, 158]
+            self.dens_treshold = [0, np.inf]
+    
+    def magnitude(self, time):
+        # If there is no more propellant, return 0
+        if self.sat.dry_mass is not None and self.vehicle.get_body_mass() <= self.sat.dry_mass:
+            return 0
+        # Return a constant thrust magnitude
+        if self.thrust_mod == 0:
+            return 0.001
+        # Return thrust from BHT 100 thruster, based on power
+        elif self.thrust_mod == 1:
+            # If there is more than a certain power available, keep it for the other systems
+            self.thrust, self.m_flow, self.I_sp = BHT_100.from_power(min(self.power, self.power_treshold[1]))
+            return self.thrust
+
+    def specific_impulse(self, time):
+        # Return the specific impulse
+        return self.Isp
+
+    def is_thrust_on(self, time):
+        """
+        Define whether the engine is on or not
+        """
+        # Engine is on if the air density is above a given treshold
+        curr_dens = self.vehicle.get_flight_conditions().current_density
+        density_ok = curr_dens > self.dens_treshold[0] and curr_dens < self.dens_treshold[1]
+        # Engine is on if the solar irradiance is above a given treshold
+        power_ok = self.power_available(time) > self.power_treshold[0]
+        return density_ok and power_ok
+
+    def power_available(self, time):
+        """
+        Compute the maximum available power from the solar panels
+        """
+        # Compute the solar irradiance (including shadowing of Mars)
+        self.solar_irradiance(time)
+        # Extract the state of the satellite and of the Sun
+        sat_state = self.vehicle.state
+        sun_state = self.sun.state
+        # Compute the power available from the solar panels
+        self.power = MG.sat_power(sat_state, sun_state, self.irradiance, [self.sat.area_x, self.sat.area_y, self.sat.area_z]) * self.sat.SA_eff * self.sat.EPS_eff
+        # If specified, save the power
+        if self.save_power:
+            power_dict[time] = self.power
+        return self.power
+
+    def solar_irradiance(self, time):
+        """
+        Compute the solar irradiance at the satellite position
+        """
+        # Get the position and radius of the required bodies
+        sun_pos = self.sun.state[:3]
+        cb_pos = self.central_body.state[:3]
+        sat_pos = self.vehicle.state[:3]
+        cb_R = spice_interface.get_average_radius(self.prop.central_body)
+        Sun_R = spice_interface.get_average_radius("Sun")
+        # Compute the shadow due to Mars between the sat and the Sun
+        shadow = MG.shadow_function(sun_pos, Sun_R, cb_pos, cb_R, sat_pos)
+        # Compute the distance between the sun and the satellite
+        sun_dist = np.linalg.norm(sun_pos-sat_pos)
+        # Compute the solar irradiance at the satellite position, based on the shadow number, and the solar constant on Earth
+        self.irradiance = self.solar_constant / (sun_dist/AU)**2 * shadow
+        # If specified, save the solar irradiance
+        if self.save_power:
+            solar_irradiances[time] = self.irradiance
+        return self.irradiance
+
+def thrust_settings(propagation, thrust_mod, save_power=False):
+    # Define the thrust guidance function
+    thrust_guidance = thrust_model(propagation, thrust_mod, save_power=save_power)
+    # Define the thrust settings (direction and magnitude)
+    thrust_direction_s = propagation_setup.acceleration.thrust_direction_from_state_guidance(
+        central_body=propagation.central_body, is_colinear_with_velocity=True, direction_is_opposite_to_vector=False)
+    thrust_magnitude_s = propagation_setup.acceleration.custom_thrust_magnitude(
+        thrust_guidance.magnitude, thrust_guidance.specific_impulse, thrust_guidance.is_thrust_on)
+    # Return the acceleration for Tudat
+    return propagation_setup.acceleration.thrust_acceleration(thrust_direction_s, thrust_magnitude_s)
