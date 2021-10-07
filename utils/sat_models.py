b3df8bd781ff2fb864d6e079176212e8f15834a6
gaffarelj <j.y.p.gaffarel@student.tudelft.nl>
2021-10-07 10:42:42 +0200

    Changed how the solar panel areas are setup

--- utils/sat_models.py
+++ utils/sat_models.py
@@ -2,25 +2,25 @@ from tudatpy.kernel.math import interpolators
 
 
 class satellite:
-    def __init__(self, name, areas, mass, Cd, Cd_h=[85e3, 115e3, 150e3], S_ref=0.01, SA_frac=0.7042, SA_eff=0.29, EPS_eff=0.93):
+    def __init__(self, name, mass, Cd, Cd_h=[85e3, 115e3, 150e3], S_ref=0.01, SA_areas=[0, 0, 0], SA_frac=0.7042, SA_eff=0.29, EPS_eff=0.93):
         """
         Satellite class, containing all relevant information for all analysed satellites configurations
         Inputs:
          * name (str): name of the satellite configuration
-         * areas ([float]*3): list containing the surface area of the satellite in each plane
          * mass ([float]*2): list containing respectively the wet and dry mass of the satellite
          * Cd ([float]*N): list containing the drag coefficient of the satellite at some altitudes
          * Cd_h ([float]*N): list at which the drag coefficients have been given
          * S_ref (float): reference surface area of the satellite
+         * SA_areas ([float]*3): list containing the solar panel surface area of the satellite in each plane
          * SA_frac (float): fraction of the satellite areas that is actually the solar array
          * SA_eff (float): efficiency of the solar array
          * EPS_eff (float): efficiency of the Electrical Power System
         """
         # Save the satellite properties
         self.name = name
-        self.area_x = areas[0]*SA_frac
-        self.area_y = areas[1]*SA_frac
-        self.area_z = areas[2]*SA_frac
+        self.area_x = SA_areas[0]*SA_frac
+        self.area_y = SA_areas[1]*SA_frac
+        self.area_z = SA_areas[2]*SA_frac
         self.wet_mass = mass[0]
         self.dry_mass = mass[1]
         self.Cd_list = Cd
@@ -53,13 +53,13 @@ class satellite:
         return self.drag_interpolator.interpolate(h)
 
 satellites = {
-    "CS_0020": satellite("CS_0020", [0, 0.042426, 0.042426], [4.29425, 3.64425], [2.83794, 2.74090, 2.80022]),
-    "CS_0021": satellite("CS_0021", [0.031058, 0.083343, 0.083343], [4.80225, 4.15225], [4.13116, 3.49154, 3.49964]),
-    "CS_1020": satellite("CS_1020", [0, 0.102426, 0.042426], [4.80225, 4.15225], [6.53680, 8.47435, 8.21728]),
-    "CS_1021": satellite("CS_1021", [0.031058, 0.143343, 0.083343], [5.31025, 4.66025], [5.33072, 4.29959, 4.24327]),
-    "CS_2020": satellite("CS_2020", [0, 0.162426, 0.042426], [5.31025, 4.66025], [7.37655, 9.09419, 8.75681]),
-    "CS_2021": satellite("CS_2021", [0.031058, 0.203343, 0.083343], [5.81825, 5.16825], [6.97704, 5.73235, 5.50560]),
-    "CS_2120": satellite("CS_2120", [0, 0.282426, 0.042426], [5.81825, 5.16825], [6.43496, 5.09859, 4.97959]),
-    "CS_3020": satellite("CS_3020", [0, 0.222426, 0.042426], [6.32625, 5.67625], [8.61881, 9.91262, 9.48819]),
-    "CS_3021": satellite("CS_3021", [0.031058, 0.263343, 0.083343], [6.32625, 5.67625], [9.77352, 10.80229, 10.21743])
+    "CS_0020": satellite("CS_0020", [4.29425, 3.64425], [2.83794, 2.74090, 2.80022], SA_areas=[0, 0.042426, 0.042426]),
+    "CS_0021": satellite("CS_0021", [4.80225, 4.15225], [4.13116, 3.49154, 3.49964], SA_areas=[0.031058, 0.083343, 0.083343]),
+    "CS_1020": satellite("CS_1020", [4.80225, 4.15225], [6.53680, 8.47435, 8.21728], SA_areas=[0, 0.102426, 0.042426]),
+    "CS_1021": satellite("CS_1021", [5.31025, 4.66025], [5.33072, 4.29959, 4.24327], SA_areas=[0.031058, 0.143343, 0.083343]),
+    "CS_2020": satellite("CS_2020", [5.31025, 4.66025], [7.37655, 9.09419, 8.75681], SA_areas=[0, 0.162426, 0.042426]),
+    "CS_2021": satellite("CS_2021", [5.81825, 5.16825], [6.97704, 5.73235, 5.50560], SA_areas=[0.031058, 0.203343, 0.083343]),
+    "CS_2120": satellite("CS_2120", [5.81825, 5.16825], [6.43496, 5.09859, 4.97959], SA_areas=[0, 0.282426, 0.042426]),
+    "CS_3020": satellite("CS_3020", [6.32625, 5.67625], [8.61881, 9.91262, 9.48819], SA_areas=[0, 0.222426, 0.042426]),
+    "CS_3021": satellite("CS_3021", [6.32625, 5.67625], [9.77352, 10.80229, 10.21743], SA_areas=[0.031058, 0.263343, 0.083343])
 }
