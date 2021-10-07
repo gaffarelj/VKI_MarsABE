d97fd07794882bcc00c7fc0f16563686df52c98f
gaffarelj <j.y.p.gaffarel@student.tudelft.nl>
2021-10-07 11:03:21 +0200

    Allowed for constant Cd and mass

--- utils/sat_models.py
+++ utils/sat_models.py
@@ -2,15 +2,15 @@ from tudatpy.kernel.math import interpolators
 
 
 class satellite:
-    def __init__(self, name, mass, Cd, Cd_h=[85e3, 115e3, 150e3], S_ref=0.01, SA_areas=[0, 0, 0], SA_frac=0.7042, SA_eff=0.29, EPS_eff=0.93):
+    def __init__(self, name, mass, Cd, S_ref=0.01, Cd_h=[85e3, 115e3, 150e3], SA_areas=[0, 0, 0], SA_frac=0.7042, SA_eff=0.29, EPS_eff=0.93):
         """
         Satellite class, containing all relevant information for all analysed satellites configurations
         Inputs:
          * name (str): name of the satellite configuration
-         * mass ([float]*2): list containing respectively the wet and dry mass of the satellite
-         * Cd ([float]*N): list containing the drag coefficient of the satellite at some altitudes
-         * Cd_h ([float]*N): list at which the drag coefficients have been given
+         * mass (float OR [float]*2): float OR list containing the constant satellite mass OR respectively the wet and dry mass of the satellite
+         * Cd (float [float]*N): float OR list containing the drag coefficient of the satellite fixed OR at some altitudes
          * S_ref (float): reference surface area of the satellite
+         * Cd_h ([float]*N): list at which the drag coefficients have been given
          * SA_areas ([float]*3): list containing the solar panel surface area of the satellite in each plane
          * SA_frac (float): fraction of the satellite areas that is actually the solar array
          * SA_eff (float): efficiency of the solar array
@@ -21,6 +21,8 @@ class satellite:
         self.area_x = SA_areas[0]*SA_frac
         self.area_y = SA_areas[1]*SA_frac
         self.area_z = SA_areas[2]*SA_frac
+        if type(mass) != list:
+            mass = [mass, mass]
         self.wet_mass = mass[0]
         self.dry_mass = mass[1]
         self.Cd_list = Cd
@@ -30,18 +32,25 @@ class satellite:
         self.EPS_eff = EPS_eff
         self.h_warning = [True, True]
         
-        # Create a cubic spline interpolator, capped at the boundaries
-        interpolator_settings = interpolators.cubic_spline_interpolation(boundary_interpolation=interpolators.use_boundary_value)
-        drag_dict = dict(zip(self.Cd_h, self.Cd_list))
-        # Setup the drag interpolator
-        self.drag_interpolator = interpolators.create_one_dimensional_interpolator(drag_dict, interpolator_settings)
+        if type(self.Cd_list) == list:
+            # Create a cubic spline interpolator, capped at the boundaries
+            interpolator_settings = interpolators.cubic_spline_interpolation(boundary_interpolation=interpolators.use_boundary_value)
+            drag_dict = dict(zip(self.Cd_h, self.Cd_list))
+            # Setup the drag interpolator
+            self.drag_interpolator = interpolators.create_one_dimensional_interpolator(drag_dict, interpolator_settings)
 
     def __str__(self):
         # Return the satellite represented as a string
-        return "Satellite %s with wet mass of %.2f kg (dry of %.2f kg), solar panel areas of %.3f/%.3f/%.3f m2, Cd ranging in (%.2f; %.2f)" % \
-            (self.name, self.wet_mass, self.dry_mass, self.area_x, self.area_y, self.area_z, min(self.Cd_list), max(self.Cd_list))
+        if type(self.Cd_list) == list:
+            cd_str = "Cd ranging in (%.2f; %.2f)" % (min(self.Cd_list), max(self.Cd_list))
+        else:
+            cd_str = "constant Cd of %.2f" % self.Cd_list
+        return "Satellite '%s' with wet mass of %.2f kg (dry of %.2f kg), reference area of %.4f m2, %s" % \
+            (self.name, self.wet_mass, self.dry_mass, self.S_ref, cd_str)
 
     def get_cd(self, h):
+        if type(self.Cd_list) != list:
+            return self.Cd_list
         # Return the interpolated drag coefficient at the given altitude h [m]
         if (h > max(self.Cd_h) and self.h_warning[0]) or (h < min(self.Cd_h) and self.h_warning[1]):
             print("Warning: the specified altitude of %i km is outside the measured range of (%i; %i) km." % (h/1e3, min(self.Cd_h)/1e3, max(self.Cd_h)/1e3))
