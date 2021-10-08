f7e2707c59282cd3a6d751b67e4b1ba8867cd8fd
gaffarelj <j.y.p.gaffarel@student.tudelft.nl>
2021-10-08 10:28:23 +0200

    Documented and clean the hall thrust test

--- thrust/test_hall_thrust.py
+++ thrust/test_hall_thrust.py
@@ -5,41 +5,30 @@ from tudatpy.kernel import constants
 from tools import plot_utilities as PU
 import numpy as np
 from utils import sat_models as SM
-
 from utils import propagation as P
 
+# Select a satellite to propagate
 satellite = SM.satellites["CS_3021"]
 sim_time = 2*constants.JULIAN_DAY
 
+# Create the orbital simulation around Mars, for two days
 OS = P.orbit_simulation(satellite, "Mars", 2*constants.JULIAN_DAY, verbose=False, save_power=True)
 
-OS.create_bodies()
-OS.create_initial_state(h=140e3)
-OS.create_termination_settings()
-OS.create_dependent_variables(to_save=["F_T", "rho", "h", "m"])
-OS.create_accelerations(default_config=1, thrust=1)
-OS.create_propagator(prop_mass=True)
-OS.create_integrator()
-
-bodies_to_propagate = [OS.sat.name]
-central_bodies = [OS.central_body]
-bodies = OS.bodies
-initial_state = OS.initial_state
-termination_settings = OS.termination_settings
-dependent_variables_to_save = OS.dependent_variables_to_save
-acceleration_settings = {OS.sat.name: OS.accelerations}
-accelerations = OS.acceleration_models
-propagator_settings = OS.propagator_settings
-integrator_settings = OS.integrator_settings
-
+OS.create_bodies()                                              # Create the simulation bodies
+OS.create_initial_state(h=140e3)                                # Start in a circular orbit at an altitude of 140km above Mars
+OS.create_termination_settings()                                # Create the settings to terminate the simulation
+OS.create_dependent_variables(to_save=["F_T", "rho", "h", "m"]) # Setup the dependent variables to be saved
+OS.create_accelerations(default_config=1, thrust=1)             # Create the acceleration models, with the hall thrust
+OS.create_propagator(prop_mass=True)                            # Setup the translational and mass propagator
+OS.create_integrator()                                          # Create the integrator
 
 # Run the simulation
 time, states, dep_vars = OS.simulate()
+# Print some states and times (to check consistency when modifying the code)
 print([states[0], states[100], states[-1]])
 print([time[0], time[100], time[-1]])
-print(dep_vars.shape)
 
-# Extract values for plotting
+# Extract results values
 positions = np.linalg.norm(states[:,:3], axis=1)
 thrust_acc = OS.get_dep_var("F_T")
 density = OS.get_dep_var("rho")
@@ -51,9 +40,9 @@ time_power = list(OS.power_dict.keys())
 time_power = (np.array(time_power) - time_power[0])/3600
 
 print("Maximum power available:", max(power_vals))
+print("Start/end mass of satellite:", sat_mass_hist[0][0], sat_mass_hist[-1][0])
 
-print("Start/end mass of satellite:", sat_mass_hist[0], sat_mass_hist[-1])
-
+# Make plots
 PU.plot_single(time_power, power_vals, "Time [hr]", "Power [W]", "thrust/power_ht", scatter=True)
 PU.plot_single(time/3600, altitude/1e3, "Time [hr]", "Altitude [m]", "thrust/alt_ht")
 PU.plot_single(states[:,0], states[:,1], "x [m]", "y [m]", "thrust/pos_ht", scatter=True, equal_ax=True)
