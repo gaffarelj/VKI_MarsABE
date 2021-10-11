397e45d42109bb634750e38e4798adbecce4c70f
gaffarelj <j.y.p.gaffarel@student.tudelft.nl>
2021-10-11 13:03:05 +0200

    Cleaned feasible altitudes code

--- MCD/feasible_altitudes.py
+++ MCD/feasible_altitudes.py
@@ -1,56 +1,41 @@
 import sys
-sys.path.insert(0,"\\".join(sys.path[0].split("\\")[:-2]))
-from tudatpy.kernel import constants
-from tudatpy.kernel.simulation import propagation_setup
-from tudatpy.kernel.interface import spice_interface
-from setup_selection import setup_utils as SU
-from tools import plot_utilities as PU
-from tools import time_conversions as TC
-import thrust.simple_thrust as T
-import matplotlib.pyplot as plt
 import numpy as np
+while sys.path[0].split("/")[-1] != "VKI_MarsABE":
+    sys.path.insert(0,"/".join(sys.path[0].split("/")[:-1]))
+from utils import propagation as P
+from utils import sat_models as SM
+from tudatpy.kernel import constants
+from tools import plot_utilities as PU
 
-#altitudes = np.arange(80, 220.1, 2.5)
-altitudes = np.arange(50, 160.1, 2.5)
-#altitudes = [85, 115, 150]
-orbit_time = []
+run_alt_study = True                    # Run the study of the altitude vs orbital time
+run_atmo_study = False                   # Run the study of the atmospheric properties at given altitudes
 
-for h in altitudes:
+sat = SM.satellite("Orbiter", 5, 3, S_ref=0.015)     # Create satellite of 5kg, with Cd of 3, and reference surface area of 0.015 m2
 
-    simulation_days = 600
-    simulation_start_epoch = TC.MCD_to_Tudat(2459942)
-    simulation_end_epoch = simulation_start_epoch + simulation_days*constants.JULIAN_DAY
+if run_alt_study:
+    altitudes = np.arange(50, 160.1, 2.5)
+    orbit_time = []
+    # Run for each altitude
+    for h in altitudes:
+        simulation_days = 600
 
-    # Define the environment and bodies
-    bodies, bodies_to_propagate, central_bodies = SU.create_bodies(use_MCD_atmo=True)
-    # Define the initial state of the satellite
-    initial_state = SU.get_initial_state(bodies, altitude=h*1e3, inclination=np.deg2rad(0.01), eccentricity=0)
-    # Define the termination settings
-    termination_settings = SU.simulation_settings(simulation_end_epoch, end_altitude=25e3)
-    # Define the dependent variables to save
-    dependent_variables_to_save = [
-        propagation_setup.dependent_variable.relative_speed("Satellite", "Mars"),
-        propagation_setup.dependent_variable.density("Satellite", "Mars")
-    ]
-    integrator_settings = SU.get_best_integrator(simulation_start_epoch)
+        # Setup the simulation
+        OS = P.orbit_simulation(sat, "Mars", simulation_days*constants.JULIAN_DAY)
+        OS.create_bodies(use_MCD=[True, False])
+        OS.create_initial_state(h=h*1e3, i=np.deg2rad(0.01))
+        OS.create_termination_settings(min_altitude=25e3)
+        OS.create_dependent_variables("rho")
+        OS.create_integrator()
+        OS.create_accelerations(default_config=1)
+        OS.create_propagator()
+        
+        # Run the simulation
+        time, states, dep_vars = OS.simulate()
+        orbit_time.append(time[-1]/constants.JULIAN_DAY)
+        print("Starting from altitude of %.1f km, stay in orbit %.1e days" % (h, time[-1]/constants.JULIAN_DAY))
 
-    accelerations = SU.setup_environment(bodies, bodies_to_propagate, central_bodies, detail_level=1)
+    # Plot the initial altitude vs orbital time
+    PU.plot_single(orbit_time, altitudes, "Time in orbit [days]", "Starting altitude [km]", "MCD/feasible_altitudes", xlog=True)
 
-    # Define the propagator settings
-    propagator_settings = propagation_setup.propagator.translational(
-        central_bodies,
-        accelerations,
-        bodies_to_propagate,
-        initial_state,
-        termination_settings,
-        propagation_setup.propagator.gauss_modified_equinoctial,
-        dependent_variables_to_save
-    )
-
-    # Run the simulation
-    time, states, dep_vars = SU.run_simulation(bodies, integrator_settings, propagator_settings, return_raw=True)
-    orbit_time.append(time[-1]/constants.JULIAN_DAY)
-    print("Starting from altitude of %.1f km, stay in orbit %.1e days" % (h, time[-1]/constants.JULIAN_DAY))
-    print("Orbital velocity of %.3f m/s, air density of %.5e kg/m3" % (np.mean(dep_vars[:10,0]), np.mean(dep_vars[:10,1])))
-
-PU.plot_single(orbit_time, altitudes, "Time in orbit [days]", "Starting altitude [km]", "MCD/feasible_altitudes", xlog=True)
+if run_atmo_study:
+    altitudes = [85, 115, 150]
