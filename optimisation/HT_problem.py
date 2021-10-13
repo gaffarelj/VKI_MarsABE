3288dd8dcdd2dcd47903129cd80c1d06854625bb
gaffarelj <j.y.p.gaffarel@student.tudelft.nl>
2021-10-13 16:30:44 +0200

    Added orbital elements design vars, added cost fun

--- optimisation/HT_problem.py
+++ optimisation/HT_problem.py
@@ -5,6 +5,7 @@ while sys.path[0].split("/")[-1] != "VKI_MarsABE":
 from utils import sat_models as SM
 from utils import propagation as P
 from tudatpy.kernel import constants
+from tudatpy.kernel.interface import spice_interface
 
 
 class HT_problem:
@@ -33,7 +34,7 @@ class HT_problem:
         Return the fitness of the given problem. This is the cost function, that Pygmo will minimise.
         """
         # Extract the individual design variables
-        h_0, test = design_variables
+        h_a_0, h_p_0, i_0, omega_0, Omega_0 = design_variables
 
         ## Setup the simulation
         # Select the satellite
@@ -43,13 +44,16 @@ class HT_problem:
         # Create the simulation bodies, and use the MCD
         OS.create_bodies(use_MCD=[False, False])
         # Create the initial state of the satellite
-        OS.create_initial_state(h=h_0)
+        R_Mars = spice_interface.get_average_radius("Mars")
+        a_0 = R_Mars + (h_a_0+h_p_0)/2
+        e_0 = 1 - (R_Mars + min(h_p_0, h_a_0)) / a_0    # Use min because h_p could actually be higher than h_a due to the way the problem is setup)
+        OS.create_initial_state(a=a_0, e=e_0, i=i_0, omega=omega_0, Omega=Omega_0)
         # Load the accelerations from default config 1: Central body spherical harmonics of degree/order 4 and aerodynamics, Solar radiation
-        OS.create_accelerations(default_config=1, thrust=0)
+        OS.create_accelerations(default_config=1, thrust=1)
         # Create the integrator, termination settings, dependent variables, and propagator
         OS.create_integrator()
         OS.create_termination_settings()
-        OS.create_dependent_variables(to_save=["h"])
+        OS.create_dependent_variables(to_save=["h_p", "h"])
         OS.create_propagator(prop_mass=False)
         
         # Simulate the satellite in orbit
@@ -57,21 +61,30 @@ class HT_problem:
 
         # Extract the results from the simulation
         power_hist = list(OS.power_dict.values())
-        altitudes = dep_vars[:,0]
-        decay = altitudes[0] - altitudes[-1]
+        h_p_s = OS.get_dep_var("h_p")
+        decay = h_p_s[0] - h_p_s[-1]
+        altitudes = OS.get_dep_var("h")
 
-        print("Start from h=%3d, sum of power=%6d W, total decay=%3d km" % (h_0/1e3, sum(power_hist), decay/1e3))
+        print("Start from h_p=%3d, h_a=%.2f, i=%2d, omega=%3d, Omega=%.3d -> mean of power=%.2f W, total decay=%4d km, mean altitude=%3d km" % \
+            (h_p_0/1e3, h_a_0/1e3, np.rad2deg(i_0), np.rad2deg(omega_0), np.rad2deg(Omega_0), np.mean(power_hist), decay/1e3, np.mean(altitudes)/1e3))
 
         ## Compute the fitness (=cost); scaling is used because, ideally, all cost values would be in the same range (0-1 for instance)
-        # Total power; lots of power = smaller value = better (use maximum observed value of 25kW to scale)
-        power_f = 25e3/sum(power_hist)
-        # Total decay; if final altitude < 50km, fitness=1 (re-entered atmosphere); else: scale with maximum 100km
-        if altitudes[-1] <= 50e3:
-            decay_f = 1
+        # Max mean power; lots of power = smaller value = better (use maximum observed value as scale)
+        power_scale = 10
+        power_f = ( (power_scale-np.mean(power_hist)) / power_scale )
+        if np.mean(power_hist) > power_scale:
+            print("Warning, mean power of %.3f W was above scaling value of %.3f W" % (np.mean(power_hist), power_scale))
+        # Min decay; if final altitude < 50km, fitness=1 (re-entered atmosphere); else: scale with maximum 100km
+        decay_f = 1 if h_p_s[-1] <= 50e3 else decay/100e3
+        # Min mean altitude
+        h_scale = [50e3, 500e3, 1e6]
+        if np.mean(altitudes) <= h_scale[1]:
+            h_f = (np.mean(altitudes) - h_scale[0]) / (h_scale[1] - h_scale[0]) * 0.75
         else:
-            decay_f = decay/100e3
-
-        cost = np.array([power_f, decay_f]) * np.array(self.fitness_weights)
+            h_f = 0.75 + 0.25 * (np.mean(altitudes) - h_scale[1]) / (h_scale[2] - h_scale[1])
+        # Assemble and return the cost
+        cost = np.array(self.fitness_weights) * np.array([power_f, decay_f, h_f]) # Mean power, periapsis decay, mean altitude
+        print("Cost =", cost)
         return cost
 
 
@@ -79,15 +92,20 @@ test = True
 if test:
     import pygmo
     # Setup the design variables range
-    min_h, max_h = 85e3, 150e3
+    min_h_p, max_h_p = 85e3, 150e3
+    min_h_a, max_h_a = 85e3, 500e3
+    min_e, max_e = 0, 0.1
+    min_i, max_i = 0, np.pi/2
+    min_omega, max_omega = 0, np.pi
+    min_Omega, max_Omega = 0, 2*np.pi
     test_a, test_b = 0, 1
     design_var_range = (
-        [min_h, test_a],
-        [max_h, test_b]
+        [min_h_p, min_h_a, min_i, min_omega, min_Omega],
+        [max_h_p, max_h_a, max_i, max_omega, max_Omega]
     )
 
     # Setup the optimisation problem
-    fitness_weights = [1, 0]
+    fitness_weights = [3, 3, 1]
     current_HT_problem = HT_problem(design_var_range, fitness_weights)
     problem = pygmo.problem(current_HT_problem)
 
@@ -98,6 +116,7 @@ if test:
 
     # Prepare variables to save optimum
     optimum_f, optimum_p, optimum_sum_f = None, None, np.inf
+    all_f, all_p = [], []
 
     # Run the optimisation
     n_generations = 1
@@ -111,11 +130,19 @@ if test:
         p = pop.get_x()
         
         # If a new optimum is reached, save it
-        opti_sum = np.min(np.sum(f, axis=0))
-        if opti_sum < optimum_sum_f:
-            idx_best = np.where(np.sum(f, axis=0) == opti_sum)[0][0]
+        fit_sum = np.min(np.sum(f, axis=0))
+        if fit_sum < optimum_sum_f:
+            idx_best = np.where(np.sum(f, axis=0) == fit_sum)[0][0]
             optimum_f, optimum_p = f[idx_best], p[idx_best]
 
-    
-    print("Optimum design variables: h_0=%3d km" % (optimum_p[0]/1e3))
+        # Save everything
+        if p not in all_p:
+            all_p.append(p)
+            all_f.append(f)
+
+    print()
+    print("Optimum initial state: h_p=%3d km, h_a=%3d km, i=%2d, omega=%3d, Omega=%.3d" % \
+        (min(optimum_p[0:1])/1e3, max(optimum_p[0:1])/1e3, np.rad2deg(optimum_p[2]), np.rad2deg(optimum_p[3]), np.rad2deg(optimum_p[4])))
     print("Resulting optimum fitness:", optimum_f)
+
+    print(all_p)
