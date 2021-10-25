ef02f3a47799728ccc2f9a184ac454ce7f106b47
gaffarelj <j.y.p.gaffarel@student.tudelft.nl>
2021-10-25 17:45:54 +0200

    Added option to open saved result files

--- optimisation/run_HT.py
+++ optimisation/run_HT.py
@@ -1,5 +1,8 @@
 import numpy as np
+from numpy.lib.index_tricks import s_
 import pygmo
+from natsort import natsorted
+import glob
 import time
 import sys
 sys.path = [p for p in sys.path if p != ""]
@@ -29,47 +32,58 @@ fitness_names = ["Mean power", "Periapsis decay", "Mean altitude"]
 current_HT_problem = HTp.HT_problem(design_var_range, fitness_weights, verbose=False)
 problem = pygmo.problem(current_HT_problem)
 
-# Select whether to run the optimisation or load the latest result file
-## TODO ************************************************************************************************************************************
+### Select whether to run the optimisation or load the latest result file ###
+run_opti = (input("Run the optimisation ? ([y]/n) (otherwise, load latest result file): ").lower().strip() in ["", "y"])
+if run_opti:    # Run a new optimisation
+    # Setup Pygmo
+    seed = 12345
+    algo_list = [
+        pygmo.algorithm(pygmo.nsga2(seed=seed)), # seems best
+        pygmo.algorithm(pygmo.moead(seed=seed, neighbours=5)),
+        pygmo.algorithm(pygmo.nspso(seed=seed)),
+        pygmo.algorithm(pygmo.ihs(seed=seed)) # maybe, but only 1 pop improved by generation (-> increase gen number)
+    ]
+    sizes = [12, 10, 10, 10]
 
-# Setup Pygmo
-seed = 12345
-algo_list = [
-    pygmo.algorithm(pygmo.nsga2(seed=seed)), # seems best
-    pygmo.algorithm(pygmo.moead(seed=seed, neighbours=5)),
-    pygmo.algorithm(pygmo.nspso(seed=seed)),
-    pygmo.algorithm(pygmo.ihs(seed=seed)) # maybe, but only 1 pop improved by generation (-> increase gen number)
-]
-sizes = [12, 10, 10, 10]
+    algo_idx = 0
+    print("Generating starting population (of size %i)..." % sizes[algo_idx])
+    pop = pygmo.population(problem, size=sizes[algo_idx], seed=seed)
+    algo = algo_list[algo_idx]
 
-algo_idx = 0
-pop = pygmo.population(problem, size=sizes[algo_idx], seed=seed)
-algo = algo_list[algo_idx]
+    opti_hist = []
 
-opti_hist = []
+    # Run the optimisation
+    n_generations = 10
+    t0 = time.time()
+    for i in range(1,n_generations+1):
+        print("Running generation %2d / %2d" % (i, n_generations))
+        # Evolve the population
+        pop = algo.evolve(pop)
+        
+        # Show time it took
+        dt, t0 = time.time() - t0, time.time()
+        print(" -> took %.1f seconds" % dt)
 
-# Run the optimisation
-n_generations = 10
-t0 = time.time()
-for i in range(1,n_generations+1):
-    print("Running generation %2d / %2d" % (i, n_generations))
-    # Evolve the population
-    pop = algo.evolve(pop)
-    
-    # Show time it took
-    dt, t0 = time.time() - t0, time.time()
-    print(" -> took %.1f seconds" % dt)
+        # Add best results to historic
+        f = np.array(pop.get_f())
+        best_f = [min(f[:,i]) for i in range(len(fitness_weights))]
+        best_f.append(min(np.mean(np.fabs(f), axis=1)))
+        opti_hist.append(best_f)
 
-    # Add best results to historic
-    f = np.array(pop.get_f())
-    best_f = [min(f[:,i]) for i in range(len(fitness_weights))]
-    best_f.append(min(np.mean(np.fabs(f), axis=1)))
-    opti_hist.append(best_f)
+    fit_results, fit_inputs, opti_hist = np.array(HTp.FIT_RESULTS), HTp.FIT_INPUTS, np.array(opti_hist)
 
-fit_results, fit_inputs, opti_hist = np.array(HTp.FIT_RESULTS), HTp.FIT_INPUTS, np.array(opti_hist)
+    # Save the results
+    np.savez(sys.path[0]+"/optimisation/results/%s_%s" % (time.strftime("%d%m%y_%H%M%S"), seed), inputs=fit_inputs, results=fit_results, opti_hist=opti_hist)
 
-# Save the results
-np.savez("optimisation/results/%s_%s" % (time.strftime("%d%m%y_%H%M%S"), seed), inputs=fit_inputs, results=fit_results, opti_hist=opti_hist)
+else:       # Load the last saved results
+    file_list = natsorted(glob.glob(sys.path[0]+"/optimisation/results/*.npz"))
+    last_results = np.load(file_list[-1])
+    fit_inputs = last_results["inputs"]
+    fit_results = last_results["results"]
+    opti_hist = last_results["opti_hist"]
+    s_names = fit_inputs[:,0].reshape((len(fit_inputs),1))
+    fit_inputs = np.array(fit_inputs[:,1:], dtype=float)
+    fit_inputs = np.concatenate([s_names, fit_inputs], axis=1, dtype=object)
 
 # Find the optimum if the sum of fitnesses is used
 idx_best = np.where(np.sum(fit_results[:,:len(fitness_weights)], axis=1) == np.min(np.sum(fit_results[:,:len(fitness_weights)], axis=1)))[0][0]
@@ -85,6 +99,6 @@ print("Resulting characteristics: mean power=%.2f W, total decay=%.2f km, mean a
 print("Explored %i different possibilities." % len(fit_inputs))
 
 # Plot the fitness progress over time
-PU.plot_multiple([list(range(1, n_generations+1))]*(len(fitness_weights)+1), opti_hist.T, "Generation number", "Best fitness", \
+PU.plot_multiple([list(range(1, opti_hist.shape[0]+1))]*(len(fitness_weights)+1), opti_hist.T, "Generation number", "Best fitness", \
     "optimisation/HT/history", legends=fitness_names+["Average fitness"], colors=["darkorange", "seagreen", "royalblue", "#202020"], \
     lstyle=["solid"]*len(fitness_weights)+["dashed"])
