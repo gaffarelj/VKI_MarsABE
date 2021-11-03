8a038f547f61caf8bdd29f5283a7bb5a1c565990
gaffarelj <j.y.p.gaffarel@student.tudelft.nl>
2021-11-03 15:06:43 +0100

    Test the implementation of the GRAM atmosphere

--- optimisation/pursue_case_with_tank.py
+++ optimisation/pursue_case_with_tank.py
@@ -13,19 +13,19 @@ h_p, h_a = 140.93e3, 235.41e3
 i, omega, Omega = np.deg2rad(81.90), np.rad2deg(173.18), np.deg2rad(166.92)
 thrust_model = 2
 
-s_name = "CS_3021"
-h_p, h_a = 115863.38, 236752.43
-i, omega, Omega = 1.0998, 0.70095, 0.40489
+# s_name = "CS_3021"
+# h_p, h_a = 115863.38, 236752.43
+# i, omega, Omega = 1.0998, 0.70095, 0.40489
 
-s_name = "CS_3021"
-h_p, h_a = 120944.9058, 87098.210909
-i, omega, Omega = 0.8070622, 0.5213307, 2.9493216
+# s_name = "CS_3021"
+# h_p, h_a = 120944.9058, 87098.210909
+# i, omega, Omega = 0.8070622, 0.5213307, 2.9493216
 
 
 # Create the orbital simulation instance, setup to simulate 5 days
-OS = P.orbit_simulation(SM.satellites_with_tank[s_name], "Mars", 5*constants.JULIAN_DAY, save_power=True)
+OS = P.orbit_simulation(SM.satellites_with_tank[s_name], "Mars", 5*constants.JULIAN_DAY, save_power=True, verbose=True)
 # Create the simulation bodies, and use the MCD
-OS.create_bodies(use_MCD=[False, False])
+OS.create_bodies(use_MCD=[True, False], use_GRAM=True)
 # Create the initial state of the satellite
 a = OS.R_cb + (h_a+h_p)/2
 e = 1 - (OS.R_cb + min(h_p, h_a)) / a       # Use min because h_p could actually be higher than h_a due to the way the problem is setup)
@@ -35,7 +35,7 @@ OS.create_accelerations(default_config=1, thrust=thrust_model)
 # Create the integrator, termination settings, dependent variables, and propagator
 OS.create_integrator()
 OS.create_termination_settings()
-OS.create_dependent_variables(to_save=["h_p", "h", "D", "F_T", "m"])
+OS.create_dependent_variables(to_save=["h_p", "h", "D", "F_T", "m", "lat", "lon"])
 OS.create_propagator(prop_mass=True)
 
 # Simulate the satellite in orbit
@@ -50,4 +50,8 @@ print(h_p_s[0] - h_p_s[-1])
 power_hist = list(OS.power_dict.values())
 print(np.mean(power_hist))
 plt.plot(times, h_p_s)
+plt.show()
+plt.plot(times, OS.get_dep_var("m"))
+plt.show()
+plt.plot(times, np.linalg.norm(OS.get_dep_var("F_T"), axis=1))
 plt.show()
