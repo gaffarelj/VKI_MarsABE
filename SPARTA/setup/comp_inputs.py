d89dc6c01fdb03b917eaaa129307d9ed3d2b65c6
gaffarelj <j.y.p.gaffarel@student.tudelft.nl>
2021-11-11 23:54:56 +0100

    Fixed error in adapt command (again)

--- SPARTA/setup/comp_inputs.py
+++ SPARTA/setup/comp_inputs.py
@@ -227,7 +227,7 @@ for j, s_name in enumerate(sat_names):
             # For the new dt, make sure the following condition is satisfied: u_ps*dt < dx
             input_s += "timestep            %.4e\n" % new_dt
             # Refine the grid where the grid Knudsen number is below 4, only in front of the satellite (coarsen it back when it is above 50)
-            input_s += "adapt_grid          all refine coarsen value c_knudsen[2] 4 50 combine min thresh less more cells %i %i %i one\n" \
+            input_s += "adapt_grid          all refine coarsen value c_knudsen[2] 4 50 combine min thresh less more cells %i %i %i\n" \
                  % tuple([refinement_factors[i_refine]]*3)
             # Increase the number of particles so that the PPC stay > ~10
             input_s += "scale_particles     all %i\n" % particles_scale[i_refine+1]
