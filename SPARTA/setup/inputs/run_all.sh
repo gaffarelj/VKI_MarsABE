3429523519c3b924c33f84179fad8181b4085925
gaffarelj <j.y.p.gaffarel@student.tudelft.nl>
2021-11-18 09:09:25 +0100

    Correctly added CS_0021 to final SPARTA inputs

--- SPARTA/setup/inputs/run_all.sh
+++ SPARTA/setup/inputs/run_all.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 module load openmpi
+mpirun -np 16 spa_ < in.CS_0021_85km | tee ../results_sparta/CS_0021/stats_85km.dat
+mpirun -np 16 spa_ < in.CS_0021_115km | tee ../results_sparta/CS_0021/stats_115km.dat
+mpirun -np 16 spa_ < in.CS_0021_150km | tee ../results_sparta/CS_0021/stats_150km.dat
 mpirun -np 16 spa_ < in.CS_1021_85km | tee ../results_sparta/CS_1021/stats_85km.dat
 mpirun -np 16 spa_ < in.CS_1021_115km | tee ../results_sparta/CS_1021/stats_115km.dat
 mpirun -np 16 spa_ < in.CS_1021_150km | tee ../results_sparta/CS_1021/stats_150km.dat
