936ea416d7ec55f3d26f3ad3a78d3d2f4cdcaa96
gaffarelj <j.y.p.gaffarel@student.tudelft.nl>
2021-11-25 18:36:38 +0100

    Make sure the entire code is under 'if main'

--- optimisation/run_problem.py
+++ optimisation/run_problem.py
@@ -111,123 +111,123 @@ if __name__ == "__main__":
             if i > 1:
                 os.remove(f_path+"-%s.npz"%(i-1))
 
-# Load the last saved results
-file_list = natsorted(glob.glob("-".join(f_path.split("-")[:-1]) + "*.npz"))
-if len(file_list) == 0:
-    raise FileNotFoundError("It appears that no optimisation was already run to at least 1 generation with the following parameters:\n"+\
-        " * thrust model = %i\n * use of battery = %s\n * ionisation efficiency = %.2f\n * population size = %i" % \
-            (thrust_model, "V" if use_battery else "X", ionisation_efficiency, pop_size))
-last_results = np.load(file_list[-1])
-fit_inputs = last_results["inputs"]
-fit_results = last_results["results"]
-opti_hist = last_results["opti_hist"]
-s_names = fit_inputs[:,0].reshape((len(fit_inputs),1))
-fit_inputs = np.array(fit_inputs[:,1:], dtype=float)
-fit_inputs = np.concatenate([s_names, fit_inputs], axis=1, dtype=object)
+    # Load the last saved results
+    file_list = natsorted(glob.glob("-".join(f_path.split("-")[:-1]) + "*.npz"))
+    if len(file_list) == 0:
+        raise FileNotFoundError("It appears that no optimisation was already run to at least 1 generation with the following parameters:\n"+\
+            " * thrust model = %i\n * use of battery = %s\n * ionisation efficiency = %.2f\n * population size = %i" % \
+                (thrust_model, "V" if use_battery else "X", ionisation_efficiency, pop_size))
+    last_results = np.load(file_list[-1])
+    fit_inputs = last_results["inputs"]
+    fit_results = last_results["results"]
+    opti_hist = last_results["opti_hist"]
+    s_names = fit_inputs[:,0].reshape((len(fit_inputs),1))
+    fit_inputs = np.array(fit_inputs[:,1:], dtype=float)
+    fit_inputs = np.concatenate([s_names, fit_inputs], axis=1, dtype=object)
 
-# Find the optimum if the sum of fitnesses is used
-idx_best = np.where(np.sum(fit_results[:,:len(fitness_weights)], axis=1) == np.min(np.sum(fit_results[:,:len(fitness_weights)], axis=1)))[0][0]
-optimum_input, optimum_result = fit_inputs[idx_best], fit_results[idx_best]
+    # Find the optimum if the sum of fitnesses is used
+    idx_best = np.where(np.sum(fit_results[:,:len(fitness_weights)], axis=1) == np.min(np.sum(fit_results[:,:len(fitness_weights)], axis=1)))[0][0]
+    optimum_input, optimum_result = fit_inputs[idx_best], fit_results[idx_best]
 
-print("Explored %i different possibilities." % len(fit_inputs))
+    print("Explored %i different possibilities." % len(fit_inputs))
 
-power_f, decay_f, h_f, D_T_f = fit_results[:,:4].T
-pareto_opt = PU.pareto_optimums([power_f, decay_f, h_f, D_T_f])
-print("There are %i Pareto optimum solutions across all four objectives." % np.sum(pareto_opt))
-print("Saving various Pareto plots...")
+    power_f, decay_f, h_f, D_T_f = fit_results[:,:4].T
+    pareto_opt = PU.pareto_optimums([power_f, decay_f, h_f, D_T_f])
+    print("There are %i Pareto optimum solutions across all four objectives." % np.sum(pareto_opt))
+    print("Saving various Pareto plots...")
 
-plots_title = "Thrust model %i, %s use of the battery" % (thrust_model, "with" if use_battery else "without")
-if thrust_model == 3:
-    plots_title += ", with ionisation efficiency of %s%%" % (ionisation_efficiency*100)
+    plots_title = "Thrust model %i, %s use of the battery" % (thrust_model, "with" if use_battery else "without")
+    if thrust_model == 3:
+        plots_title += ", with ionisation efficiency of %s%%" % (ionisation_efficiency*100)
 
-# Plot the fitness progress over time
-PU.plot_multiple([list(range(1, opti_hist.shape[0]+1))]*(len(fitness_weights)+1), opti_hist.T, "Generation number", "Best fitness", \
-    plots_path+"history", legends=fitness_names+["Average fitness"], colors=["darkorange", "seagreen", "royalblue", "#202020"], \
-    lstyle=["solid"]*len(fitness_weights)+["dashed"], title=plots_title)
+    # Plot the fitness progress over time
+    PU.plot_multiple([list(range(1, opti_hist.shape[0]+1))]*(len(fitness_weights)+1), opti_hist.T, "Generation number", "Best fitness", \
+        plots_path+"history", legends=fitness_names+["Average fitness"], colors=["darkorange", "seagreen", "royalblue", "#202020"], \
+        lstyle=["solid"]*len(fitness_weights)+["dashed"], title=plots_title)
 
-# Generate Pareto fronts
-for zoomed in [False]:#, True]:
-    if zoomed:
-        # Zoom in on region where mean altitude is below 150km and where periapsis decay is within -5km to 5km
-        idx_remove = np.where((fit_results[:,-3] > 5e3) | (fit_results[:,-3] < -5e3) | (fit_results[:,-2] > 150e3))
-        prefix = "_zoomed"
-    else:
-        # Filter periapsis decays above 100km
-        idx_remove = np.where(fit_results[:,-3] > 100e3)
-        prefix = ""
-    # Remove at selected indexes
-    obj_power, obj_decay, obj_h, obj_TD = np.delete(fit_results[:,-4], idx_remove), np.delete(fit_results[:,-3], idx_remove), np.delete(fit_results[:,-2], idx_remove), np.delete(fit_results[:,-1], idx_remove)
-    s_names = np.delete(fit_inputs[:,0], idx_remove)
-    # Select color as a function of the satellite
-    s_numbers = np.arange(0, len(satellites), 1)
-    s_nn_map = dict(zip(list(satellites.keys()), s_numbers))
-    ## Make the plots
-    # Classic Pareto plots, 2 objectives
-    PU.plot_single(obj_power, obj_decay/1e3, "Mean Power [W]", "Periapsis decay [km]", plots_path+"Pareto_Pd"+prefix, \
-        scatter=True, add_front=True, front_sign=[-1,1], title=plots_title)
-    PU.plot_single(obj_power, obj_h/1e3, "Mean Power [W]", "Mean altitude [km]", plots_path+"Pareto_Ph"+prefix, \
-        scatter=True, add_front=True, front_sign=[-1,1], title=plots_title)
-    PU.plot_single(obj_h/1e3, obj_decay/1e3, "Mean altitude [km]", "Periapsis decay [km]", plots_path+"Pareto_hd"+prefix, \
-        scatter=True, add_front=True, title=plots_title)
-    PU.plot_single(obj_h/1e3, obj_TD, "Mean altitude [km]", "Mean Thrust/Drag [-]", plots_path+"Pareto_hT"+prefix, \
-        scatter=True, add_front=True, front_sign=[1,-1], title=plots_title)
-    PU.plot_single(obj_power, obj_TD, "Mean Power [W]", "Mean Thrust/Drag [-]", plots_path+"Pareto_PT"+prefix, \
-        scatter=True, add_front=True, front_sign=[-1,-1], title=plots_title)
-    PU.plot_single(obj_TD, obj_decay/1e3, "Mean Thrust/Drag [-]", "Periapsis decay [km]", plots_path+"Pareto_Td"+prefix, \
-        scatter=True, add_front=True, front_sign=[-1,1], title=plots_title)
-    # Plot decay vs mean altitude with power in the colormap
-    power_cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
-                    'trunc({n},{a:.2f},{b:.2f})'.format(n="plasma", a=0.0, b=0.9),
-                    matplotlib.pyplot.get_cmap("plasma")(np.linspace(0.0, 0.9, 10)))
-    PU.plot_single(obj_h/1e3, obj_decay/1e3, "Mean altitude [km]", "Periapsis decay [km]", plots_path+"Pareto_hdP"+prefix, \
-        scatter=True, add_front=True, z_data=obj_power, z_label="Mean power [W]", cmap=power_cmap, title=plots_title)
-    # Plot decay vs mean altitude with satellite name in the colormap
-    s_name_cmap = matplotlib.colors.ListedColormap(['red', 'green', 'blue', 'yellow', 'orange'])
-    bounds = [0, 1, 2, 3, 4, 5]
-    norm =  matplotlib.colors.BoundaryNorm(bounds, s_name_cmap.N)
-    PU.plot_single(obj_h/1e3, obj_decay/1e3, "Mean altitude [km]", "Periapsis decay [km]", plots_path+"Pareto_hdS"+prefix, \
-        scatter=True, add_front=True, z_data=[s_nn_map[s_n] for s_n in s_names], z_label="Satellite", \
-            cmap=s_name_cmap, cticks=[0.5, 1.5, 2.5, 3.5, 4.5], \
-            clabels=list(satellites.keys()), NB=(norm, bounds), title=plots_title)
-    # Plot decay vs mean altitude with D/T in the colormap
-    PU.plot_single(obj_h/1e3, obj_decay/1e3, "Mean altitude [km]", "Periapsis decay [km]", plots_path+"Pareto_hdT"+prefix, \
-        scatter=True, add_front=True, z_data=np.clip(obj_TD, 0, 10), z_label="Mean Thrust/Drag [-]", cmap="rainbow", title=plots_title)
+    # Generate Pareto fronts
+    for zoomed in [False]:#, True]:
+        if zoomed:
+            # Zoom in on region where mean altitude is below 150km and where periapsis decay is within -5km to 5km
+            idx_remove = np.where((fit_results[:,-3] > 5e3) | (fit_results[:,-3] < -5e3) | (fit_results[:,-2] > 150e3))
+            prefix = "_zoomed"
+        else:
+            # Filter periapsis decays above 100km
+            idx_remove = np.where(fit_results[:,-3] > 100e3)
+            prefix = ""
+        # Remove at selected indexes
+        obj_power, obj_decay, obj_h, obj_TD = np.delete(fit_results[:,-4], idx_remove), np.delete(fit_results[:,-3], idx_remove), np.delete(fit_results[:,-2], idx_remove), np.delete(fit_results[:,-1], idx_remove)
+        s_names = np.delete(fit_inputs[:,0], idx_remove)
+        # Select color as a function of the satellite
+        s_numbers = np.arange(0, len(satellites), 1)
+        s_nn_map = dict(zip(list(satellites.keys()), s_numbers))
+        ## Make the plots
+        # Classic Pareto plots, 2 objectives
+        PU.plot_single(obj_power, obj_decay/1e3, "Mean Power [W]", "Periapsis decay [km]", plots_path+"Pareto_Pd"+prefix, \
+            scatter=True, add_front=True, front_sign=[-1,1], title=plots_title)
+        PU.plot_single(obj_power, obj_h/1e3, "Mean Power [W]", "Mean altitude [km]", plots_path+"Pareto_Ph"+prefix, \
+            scatter=True, add_front=True, front_sign=[-1,1], title=plots_title)
+        PU.plot_single(obj_h/1e3, obj_decay/1e3, "Mean altitude [km]", "Periapsis decay [km]", plots_path+"Pareto_hd"+prefix, \
+            scatter=True, add_front=True, title=plots_title)
+        PU.plot_single(obj_h/1e3, obj_TD, "Mean altitude [km]", "Mean Thrust/Drag [-]", plots_path+"Pareto_hT"+prefix, \
+            scatter=True, add_front=True, front_sign=[1,-1], title=plots_title)
+        PU.plot_single(obj_power, obj_TD, "Mean Power [W]", "Mean Thrust/Drag [-]", plots_path+"Pareto_PT"+prefix, \
+            scatter=True, add_front=True, front_sign=[-1,-1], title=plots_title)
+        PU.plot_single(obj_TD, obj_decay/1e3, "Mean Thrust/Drag [-]", "Periapsis decay [km]", plots_path+"Pareto_Td"+prefix, \
+            scatter=True, add_front=True, front_sign=[-1,1], title=plots_title)
+        # Plot decay vs mean altitude with power in the colormap
+        power_cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
+                        'trunc({n},{a:.2f},{b:.2f})'.format(n="plasma", a=0.0, b=0.9),
+                        matplotlib.pyplot.get_cmap("plasma")(np.linspace(0.0, 0.9, 10)))
+        PU.plot_single(obj_h/1e3, obj_decay/1e3, "Mean altitude [km]", "Periapsis decay [km]", plots_path+"Pareto_hdP"+prefix, \
+            scatter=True, add_front=True, z_data=obj_power, z_label="Mean power [W]", cmap=power_cmap, title=plots_title)
+        # Plot decay vs mean altitude with satellite name in the colormap
+        s_name_cmap = matplotlib.colors.ListedColormap(['red', 'green', 'blue', 'yellow', 'orange'])
+        bounds = [0, 1, 2, 3, 4, 5]
+        norm =  matplotlib.colors.BoundaryNorm(bounds, s_name_cmap.N)
+        PU.plot_single(obj_h/1e3, obj_decay/1e3, "Mean altitude [km]", "Periapsis decay [km]", plots_path+"Pareto_hdS"+prefix, \
+            scatter=True, add_front=True, z_data=[s_nn_map[s_n] for s_n in s_names], z_label="Satellite", \
+                cmap=s_name_cmap, cticks=[0.5, 1.5, 2.5, 3.5, 4.5], \
+                clabels=list(satellites.keys()), NB=(norm, bounds), title=plots_title)
+        # Plot decay vs mean altitude with D/T in the colormap
+        PU.plot_single(obj_h/1e3, obj_decay/1e3, "Mean altitude [km]", "Periapsis decay [km]", plots_path+"Pareto_hdT"+prefix, \
+            scatter=True, add_front=True, z_data=np.clip(obj_TD, 0, 10), z_label="Mean Thrust/Drag [-]", cmap="rainbow", title=plots_title)
 
-idx_remove = np.where(fit_results[:,-3] >= 100e3)
-# Make a Panda dataframe from the results
-import pandas as pd
-import plotly.express as px
-s_pd = pd.Series(np.delete(fit_results[:,-3], idx_remove)/1e3, name="Periapsis decay [km]")
-s_mh = pd.Series(np.delete(fit_results[:,-2], idx_remove)/1e3, name="Mean altitude [km]")
-s_mTD = pd.Series(np.delete(fit_results[:,-1], idx_remove), name="Mean T/D [-]")
-s_mp = pd.Series(np.delete(fit_results[:,-4], idx_remove), name="Mean power [W]")
-s_sn = pd.Series(np.delete(fit_inputs[:,0], idx_remove), name="Satellite")
-s_i_hp = pd.Series(np.delete(fit_inputs[:,1], idx_remove)/1e3, name="Initial h_p [km]")
-s_i_ha = pd.Series(np.delete(fit_inputs[:,2], idx_remove)/1e3, name="Initial h_a [km]")
-s_i_i = pd.Series(np.delete(fit_inputs[:,3], idx_remove)*180/np.pi, name="Initial i [deg]")
-s_i_omega = pd.Series(np.delete(fit_inputs[:,4], idx_remove)*180/np.pi, name="Initial omega [deg]")
-s_i_Omega = pd.Series(np.delete(fit_inputs[:,5], idx_remove)*180/np.pi, name="Initial Omega [deg]")
-s_pareto_opt = pd.Series(np.delete(pareto_opt, idx_remove), name="Pareto optimum")
-s_f_P = pd.Series(np.delete(power_f, idx_remove), name="Power fitness [-]")
-s_f_D = pd.Series(np.delete(decay_f, idx_remove), name="Periapsis decay fitness [-]")
-s_f_H = pd.Series(np.delete(h_f, idx_remove), name="Altitude fitness [-]")
-s_f_TD = pd.Series(np.delete(D_T_f, idx_remove), name="T/D fitness [-]")
-df = pd.concat([s_pd, s_mh, s_mTD, s_mp, s_sn, s_i_hp, s_i_ha, s_i_i, s_i_omega, s_i_Omega, s_pareto_opt, s_f_P, s_f_D, s_f_H, s_f_TD], axis=1)
-# Make interactive plot
-fig = px.scatter(df, hover_data={
-    "Initial h_p [km]": ":.7f",
-    "Initial h_a [km]": ":.7f",
-    "Initial i [deg]": ":.7f",
-    "Initial omega [deg]": ":.7f",
-    "Initial Omega [deg]": ":.7f",
-    "Mean power [W]": ":.3f",
-    "Periapsis decay [km]": ":.3f",
-    "Mean altitude [km]": ":.3f",
-    "Mean T/D [-]": ":.5f",
-    "Power fitness [-]": ":.5f",
-    "Periapsis decay fitness [-]": ":.5f",
-    "Altitude fitness [-]": ":.5f",
-    "T/D fitness [-]": ":.5f"}, \
-    x="Mean altitude [km]", y="Periapsis decay [km]", hover_name="Satellite", \
-    title=plots_title, color="Pareto optimum", color_discrete_map={True: "#2e7d32", False: "#b71c1c"})
-fig.write_html(sys.path[0]+"/figures/"+plots_path+"interactive_pareto.html")
+    idx_remove = np.where(fit_results[:,-3] >= 100e3)
+    # Make a Panda dataframe from the results
+    import pandas as pd
+    import plotly.express as px
+    s_pd = pd.Series(np.delete(fit_results[:,-3], idx_remove)/1e3, name="Periapsis decay [km]")
+    s_mh = pd.Series(np.delete(fit_results[:,-2], idx_remove)/1e3, name="Mean altitude [km]")
+    s_mTD = pd.Series(np.delete(fit_results[:,-1], idx_remove), name="Mean T/D [-]")
+    s_mp = pd.Series(np.delete(fit_results[:,-4], idx_remove), name="Mean power [W]")
+    s_sn = pd.Series(np.delete(fit_inputs[:,0], idx_remove), name="Satellite")
+    s_i_hp = pd.Series(np.delete(fit_inputs[:,1], idx_remove)/1e3, name="Initial h_p [km]")
+    s_i_ha = pd.Series(np.delete(fit_inputs[:,2], idx_remove)/1e3, name="Initial h_a [km]")
+    s_i_i = pd.Series(np.delete(fit_inputs[:,3], idx_remove)*180/np.pi, name="Initial i [deg]")
+    s_i_omega = pd.Series(np.delete(fit_inputs[:,4], idx_remove)*180/np.pi, name="Initial omega [deg]")
+    s_i_Omega = pd.Series(np.delete(fit_inputs[:,5], idx_remove)*180/np.pi, name="Initial Omega [deg]")
+    s_pareto_opt = pd.Series(np.delete(pareto_opt, idx_remove), name="Pareto optimum")
+    s_f_P = pd.Series(np.delete(power_f, idx_remove), name="Power fitness [-]")
+    s_f_D = pd.Series(np.delete(decay_f, idx_remove), name="Periapsis decay fitness [-]")
+    s_f_H = pd.Series(np.delete(h_f, idx_remove), name="Altitude fitness [-]")
+    s_f_TD = pd.Series(np.delete(D_T_f, idx_remove), name="T/D fitness [-]")
+    df = pd.concat([s_pd, s_mh, s_mTD, s_mp, s_sn, s_i_hp, s_i_ha, s_i_i, s_i_omega, s_i_Omega, s_pareto_opt, s_f_P, s_f_D, s_f_H, s_f_TD], axis=1)
+    # Make interactive plot
+    fig = px.scatter(df, hover_data={
+        "Initial h_p [km]": ":.7f",
+        "Initial h_a [km]": ":.7f",
+        "Initial i [deg]": ":.7f",
+        "Initial omega [deg]": ":.7f",
+        "Initial Omega [deg]": ":.7f",
+        "Mean power [W]": ":.3f",
+        "Periapsis decay [km]": ":.3f",
+        "Mean altitude [km]": ":.3f",
+        "Mean T/D [-]": ":.5f",
+        "Power fitness [-]": ":.5f",
+        "Periapsis decay fitness [-]": ":.5f",
+        "Altitude fitness [-]": ":.5f",
+        "T/D fitness [-]": ":.5f"}, \
+        x="Mean altitude [km]", y="Periapsis decay [km]", hover_name="Satellite", \
+        title=plots_title, color="Pareto optimum", color_discrete_map={True: "#2e7d32", False: "#b71c1c"})
+    fig.write_html(sys.path[0]+"/figures/"+plots_path+"interactive_pareto.html")
